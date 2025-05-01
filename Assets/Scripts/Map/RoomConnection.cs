c47a042c96cc13546027e3e0ae8d85ac1960019b
GerardBlasco <a24gerblaper@inspedralbes.cat>
2025-05-01 15:23:34 +0200

    Añadida animacion de transicion entre habitaciones.

--- Assets/Scripts/Map/RoomConnection.cs
+++ Assets/Scripts/Map/RoomConnection.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RoomConnectionController : MonoBehaviour
 {
     [SerializeField]
     GameObject destination;
-
-    void Start()
-    {
-        Vector3 destLocation = destination.transform.position;
-    }
+    [SerializeField]
+    Animator anim;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        anim.SetTrigger("Start");
         collision.transform.position = destination.transform.position;
+        anim.SetTrigger("End");
     }
 }
