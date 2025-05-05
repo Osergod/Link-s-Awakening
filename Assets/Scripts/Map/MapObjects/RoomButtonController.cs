7e6bd18f29a58e6c4489da88377b09b66aaa5315
GerardBlasco <a24gerblaper@inspedralbes.cat>
2025-05-05 19:52:43 +0200

    Añadidas puertas que se abren a través de un factor externo.

--- Assets/Scripts/Map/MapObjects/RoomButtonController.cs
+++ Assets/Scripts/Map/MapObjects/RoomButtonController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class RoomButtonController : MonoBehaviour
 {
     [SerializeField] Sprite pressedSprite;
+    [SerializeField] BlockedDoorController targetObject;
 
     private bool isPressed = false;
 
@@ -15,6 +16,7 @@ public class RoomButtonController : MonoBehaviour
         {
             isPressed = true;
             gameObject.GetComponent<SpriteRenderer>().sprite = pressedSprite;
+            targetObject.Activate();
         }
     }
 
