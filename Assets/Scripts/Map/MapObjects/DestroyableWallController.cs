61e80a029bfdd24895c26f6be26125e356c6102c
GerardBlasco <a24gerblaper@inspedralbes.cat>
2025-05-08 19:02:07 +0200

    Añadida pared que se destruye con explosiones.

---
+++ Assets/Scripts/Map/MapObjects/DestroyableWallController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyableWallController : ActionableMapObject
+{
+    [SerializeField] GameObject effect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Explosion")
+        {
+            Activate();
+        }
+    }
+
+    public override void Activate()
+    {
+        Instantiate(effect, transform.position, Quaternion.identity);
+        this.
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<PolygonCollider2D>().enabled = false;
+    }
+}
