4fe92222869ee7c9dacbe207acfb99f9a4d3f333
GerardBlasco <a24gerblaper@inspedralbes.cat>
2025-05-11 17:14:29 +0200

    Añadido enemigo Spark

---
+++ Assets/Scripts/Enemies/SparkController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SparkController : Enemy
+{
+    [SerializeField] float speed;
+    [SerializeField] List<GameObject> targetPoints = new List<GameObject>();
+
+    int nextTagetPoint = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Move();
+        Debug.Log(nextTagetPoint);
+    }
+
+    public void Move()
+    {
+        if (nextTagetPoint == targetPoints.Count)
+        {
+            nextTagetPoint = 0;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPoints[nextTagetPoint].transform.position, 2f * Time.deltaTime);
+
+        if (transform.position == targetPoints[nextTagetPoint].transform.position)
+        {
+            nextTagetPoint++;
+        }
+    }
+
+    public override void Attack()
+    {
+        
+    }
+}
