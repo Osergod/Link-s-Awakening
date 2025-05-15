6a8f55884ae57cfd5064915c1fe87513fa583393
Paula-LL <plizelucea@gmail.com>
2025-05-15 19:14:41 +0200

    Esperando a Singleton para el txt

---
+++ Assets/Scripts/Files/PlayerInfo.cs
@@ -0,0 +1,10 @@
+using System.Data;
+
+[System.Serializable]
+public class PlayerInfo
+{
+    public int PlayerID;
+    public string PlayerName;
+    public int PlayerScore;
+    public float PlayerRunTime;
+}
